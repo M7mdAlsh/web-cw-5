bde5c7f96a08a47ed99fd7648bf45445bdb4ee88
M7mdAlsh <xxmohammadx123@hotmail.com>
2022-03-16 20:51:23 +0300

    done

--- scrips.js
+++ scrips.js
@@ -11,3 +11,19 @@ console.log(budget > 30 && budget < 80);
 budget = 20 + 30;
 
 console.log(budget > 30 && budget < 80);
+
+let grade = prompt("كم نسبتك؟");
+
+console.log(grade);
+
+if (grade >= 90) {
+  console.log("لقد حصلت على امتياز 🥳");
+} else if (grade >= 80) {
+  console.log("لقد حصلت على جيد🙂");
+} else if (grade >= 60) {
+  console.log("لقد حصلت على مقبول😕");
+} else if (grade >= 50) {
+  console.log("لقد حصلت على ضعيف☹️");
+} else if (grade <= 50) {
+  console.log("راسب💔");
+}
