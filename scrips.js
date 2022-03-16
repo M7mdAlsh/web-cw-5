24801529e9f7ebe5dd2f4b9bad1442c9df4d1078
M7mdAlsh <xxmohammadx123@hotmail.com>
2022-03-16 19:34:48 +0300

    done

--- scrips.js
+++ scrips.js
@@ -3,3 +3,11 @@ alert(`محمد الشمري`);
 console.log(`14`);
 
 console.log("%c انا بالصف التاسع", "color: #1c87c9; font-size: 18px");
+
+let budget = 20;
+
+console.log(budget > 30 && budget < 80);
+
+budget = 20 + 30;
+
+console.log(budget > 30 && budget < 80);
